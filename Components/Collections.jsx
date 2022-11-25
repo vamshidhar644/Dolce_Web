509fe038fed3e125977bd77b494dfa07f95fbae2
VAMSHIDHAR DAWOOR <vamshidhar.20bce7285@vitap.ac.in>
2022-11-26 00:54:45 +0530

    Error Rectified

--- Components/Collections.jsx
+++ Components/Collections.jsx
@@ -1,23 +1,23 @@
-import React from 'react'
-import styles from '../styles/Collections.module.css'
-import Link from 'next/link'
-import { urlFor } from '../lib/client'
+import React from 'react';
+import styles from '../styles/Collections.module.css';
+import Link from 'next/link';
+import { urlFor } from '../dolce/lib/client';
 
-const Collections = ({collectionscards : {price, slug, cardImage, cardText}}) => {
-  return ( 
-    <div className={styles.CollectionsCard}> 
+const Collections = ({
+  collectionscards: { price, slug, cardImage, cardText },
+}) => {
+  return (
+    <div className={styles.CollectionsCard}>
       <Link href={`/collection/${slug.current}`}>
         <div className={styles.CardImage}>
-          <img 
-              src={urlFor(cardImage && cardImage[0])}
-            />
+          <img src={urlFor(cardImage && cardImage[0])} />
         </div>
-          <p className={styles.p}>{cardText}</p>
-          <p className={styles.price}>Rs. {price}</p>
-      </Link>    
+        <p className={styles.p}>{cardText}</p>
+        <p className={styles.price}>Rs. {price}</p>
+      </Link>
       {/* {console.log(price)}  */}
     </div>
-  )
-}
+  );
+};
 
-export default Collections
+export default Collections;
--- Components/Intro.jsx
+++ Components/Intro.jsx
@@ -1,22 +1,19 @@
-import React from 'react'
-import styles from '../styles/Intro.module.css' 
-import Link from 'next/link'
-import { urlFor } from '../lib/client'
- 
-const Intro = ({homecards: {name, slug, cardImage, cardText}}) => {
+import React from 'react';
+import styles from '../styles/Intro.module.css';
+import Link from 'next/link';
+import { urlFor } from '../dolce/lib/client';
+
+const Intro = ({ homecards: { name, slug, cardImage, cardText } }) => {
   return (
-    <div className={styles.HomeCards}> 
+    <div className={styles.HomeCards}>
       <Link href={`/intro/${slug.current}`}>
         <div className={styles.CardImage}>
-          <img 
-              src={urlFor(cardImage && cardImage[0])}
-            />
-        </div>       
-          <p className={styles.p}>{cardText}</p>
-      </Link>     
+          <img src={urlFor(cardImage && cardImage[0])} />
+        </div>
+        <p className={styles.p}>{cardText}</p>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-
-export default Intro
+export default Intro;
